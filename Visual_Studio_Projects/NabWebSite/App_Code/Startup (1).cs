d2affa179cf08e663ba0e852bca949d31e9e5926
Nabil Manzoor <nabilpk@gmail.com>
2015-03-04 14:52:16 +0500

    Dreamweavar and Visual Studio files

---
+++ Visual_Studio_Projects/NabWebSite/App_Code/Startup (1).cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NabWebSite.Startup))]
+namespace NabWebSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
